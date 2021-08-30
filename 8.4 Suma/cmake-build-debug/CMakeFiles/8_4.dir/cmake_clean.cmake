e604f2faf41c164f6345fbf61e4fd78fba23afda
JanuszWdowka <56833611+JanuszWdowka@users.noreply.github.com>
2021-08-30 19:52:36 +0200

    Add files via upload

---
+++ 8.4 Suma/cmake-build-debug/CMakeFiles/8_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "8_4"
+  "8_4.pdb"
+  "CMakeFiles/8_4.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/8_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
